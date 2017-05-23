4e2011aec55332bda7f7a4456d744d54f2e9ebce
Maythee Anegboonlap <null@llun.me>
2017-05-23 22:45:49 +0800

    Add build script and configuration for register/renew lambda function

--- functions/register/index.js
+++ functions/register/index.js
@@ -7,10 +7,15 @@ const s3Challenge = require('le-challenge-s3')
 const configs = {
   bucketName: process.env.BUCKET
 }
+const challenge = s3Challenge.create({ S3: configs })
 const instance = LE.create({
   server: process.env.LETSENCRYPT_SERVER || LE.stagingServerUrl,
-  store: s3Store.create({ S3: configs }),
-  challenges: { 'http-01': s3Challenge.create({ S3: configs })},
+  store: s3Store.create({ S3: configs, debug: true }),
+  challenges: {
+    'http-01': challenge,
+    'tls-sni-01': challenge,
+    'tls-sni-02': challenge
+  },
   challengeType: 'http-01',
   agreeToTerms({ tosUrl }, callback) {
     callback(null, tosUrl)
@@ -19,13 +24,18 @@ const instance = LE.create({
 
 exports.handle = (event, context, callback) => {
   const { domain, email } = event
+  if (!domain &&  !email) {
+    return callback(new Error('required domain and email'))
+  }
+
+  console.log(`registering ${domain} with account ${email}`)
   instance.register({
     domains: [ domain ],
     email: email,
     agreeTos: true,
     rsaKeySize: 2048,
     challengeType: 'http-01'
-  }).then(() => {
-    callback({ ok: true })
-  }).catch(error => callback(error))
+  })
+  .then(() => callback(null, { ok: true }))
+  .catch(error => callback(error))
 }
